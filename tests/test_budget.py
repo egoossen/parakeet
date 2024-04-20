0c3e457778735bfee3aef951df75957db7318a7c
Elijah Goossen <ekgoossen@gmail.com>
2024-04-20 07:16:21 -0500

    Adds budget class.

---
+++ tests/test_budget.py
@@ -0,0 +1,10 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+# Copyright (C) 2024 Elijah Goossen, ekgoossen@gmail.com
+
+from parakeet.budget import Budget
+
+def test_create_budget():
+    budget = Budget()
